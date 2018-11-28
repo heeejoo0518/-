14540461dae0bdceeb921deb972b967ea3ef97e4
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2018-11-28 22:39:40 +0900

    BST
    가독성 위해 조금 수정

--- BST.cpp
+++ BST.cpp
@@ -52,6 +52,7 @@ int main() {
 		newNode = new node; 
 		return newNode; 
 	}
+
 	int height(node* tree) { 
 		if (tree == NULL) return -1; 
 		int left_h = height(tree->left) + 1; 
@@ -59,24 +60,29 @@ int main() {
 		if (left_h > right_h) return left_h;
 		else return right_h; 
 	}
+
 	int noNodes(node* tree) { 
 		if (tree == NULL) return 0;
 		return noNodes(tree->left) + noNodes(tree->right) + 1; 
 	}
+
 	node* maxNode(node* tree) { 
 		if (tree->right == NULL) return tree; 
 		return maxNode(tree->right); 
 	}
+
 	node* minNode(node* tree) { 
 		if (tree->left == NULL) return tree; 
 		return minNode(tree->left);
 	}
+
 	void inorder(node* tree) { 
 		if (tree == NULL) return; 
 		inorder(tree->left); 
 		if (tree->key != 0) cout << tree->key << " "; 
 		inorder(tree->right);
 	}
+
 	void insertBST(node* tree, int newKey) { 
 		node* p = tree;
 		node* q = NULL;
@@ -89,26 +95,34 @@ int main() {
 		node* newNode = getNode(); 
 		newNode->key = newKey; 
 		newNode->left = NULL; 
-		newNode->right = NULL; 
+		newNode->right = NULL;
+
 		if (tree == NULL) {
 			tree = newNode;
-		} else if (newKey < q->key) q->left = newNode; 
+		} 
+
+		else if (newKey < q->key) q->left = newNode; 
 		else q->right = newNode; return;
 	}
 	
 	void deleteBST(node* tree, int deleteKey) {
 		node* q = NULL; 
 		node* p = Tree; 
+
 		while (p != NULL) { 
 			if (deleteKey == p->key) break; 
 			q = p;  
 			if (deleteKey < p->key) p = p->left; 
 			else p = p->right; 
-		} if (p == NULL) return; 
+		} 
+
+		if (p == NULL) return; 
+
 		if (q == NULL && p->left == NULL && p->right == NULL) { 
 			p->key = NULL; 
 			return;
 		}
+
 		if (p->left == NULL && p->right == NULL) { 
 			if (q->left == p) q->left = NULL;  
 			else q->right = NULL; 
@@ -118,21 +132,28 @@ int main() {
 				if (q == NULL) { 
 					p->key = p->left->key;    
 					p->left = NULL; 
-				} else if (q->left == p) q->left = p->left;   
+				} 
+				else if (q->left == p) q->left = p->left;   
 				else q->right = p->left; 
-			} else { 
+			} 
+			else { 
 				if (q == NULL) { 
 					p->key = p->right->key;    
 					p->right = NULL; 
-				} else if (q->left == p) q->left = p->right;   
-				else q->right = p->right; } }
+				} 
+				else if (q->left == p) q->left = p->right;   
+				else q->right = p->right; 
+			} 
+		}
 		else {
 			node* r;  bool flag;  
 			int left_h = height(p->left);  
 			int right_h = height(p->right);
+
 			if (left_h > right_h) { 
 				r = maxNode(p->left);   
-				flag = true; }
+				flag = true;
+			}
 			else if (left_h < right_h) { 
 				r = minNode(p->right);  
 				flag = false;
@@ -140,14 +161,17 @@ int main() {
 			else {
 				int left_no = noNodes(p->left);   
 				int right_no = noNodes(p->right);   
-				if (left_no >= right_no) { r = maxNode(p->left);    
-				flag = true;
-				} else { 
+				if (left_no >= right_no) { 
+					r = maxNode(p->left);    
+					flag = true;
+				} 
+				else { 
 					r = minNode(p->right);   
 					flag = false; 
 				}
 			}    
 			if (flag) deleteBST(p->left, r->key);  
-			else deleteBST(p->right, r->key);  p->key = r->key;
+			else deleteBST(p->right, r->key); 
+			p->key = r->key;
 		}
 	}
